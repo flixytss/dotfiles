3eb649073a88742ea3bc98a4f94642b428a9cee7
Daniel Alejandro <flixytssyutu@gmail.com>
2026-03-06 19:45:41 -0400

    .

--- install.sh
+++ install.sh
@@ -1,11 +1,24 @@
 # !/bin/sh
 
-sudo pacman -S nautilus alacritty cmake make
+sudo pacman -S nautilus alacritty cmake make lua
 
-mv .config/waybar ~/.config/waybar
-mv .config/hypr ~/.config/hypr
-mv swww-wallpaper.py ~/.local/share/nautilus-python/extensions
+cp .config/waybar ~/.config/waybar
+cp .config/hypr ~/.config/hypr
+
+git clone https://github.com/flixytss/swww-setter-general.git setter
+git clone https://github.com/flixytss/MainColor.git main-color
+git clone https://github.com/flixytss/nautilus-plugin-swww.git plugin
 
 cd main-color
+mkdir build
 make all
 make install
+cd ..
+cd setter
+mkdir build
+make all
+make install
+cd ..
+cd plugin
+cp plugin.py ~/.local/share/nautilus-python/extensions/swww-wallpaper.py
+cd ..
